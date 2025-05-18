9690f1638c48271b9bdb2c6e18270f0e58343ab6
Md Faizan <mdfaizankurisaray@gmail.com>
2025-05-18 13:55:05 +0530

    second commit

---
+++ code2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 10;
+
+    a += 5;             
+    cout<< a <<endl;     //a = a + 5 => 15
+    a -= 5;             
+    cout<< a <<endl;     // a = a - 5 => 10
+    a *= 5;
+    cout<< a <<endl;      // a = a * 5 => 50
+    a /= 5;
+    cout<< a <<endl;      // a = a / 5 => 10
+
+
+    return 0;
+}
